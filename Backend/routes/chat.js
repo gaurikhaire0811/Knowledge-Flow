a8a720ba2aecd1e692068e6ccae21ba86dc2a68e
Gauri Khaire <gaurikhaire191@gmail.com>
2026-09-18 10:35:38 +0530

    Fix Thread model import

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -2,7 +2,7 @@
 import express from "express";
 import passport from "passport";
 import User from "../models/user.js";
-import Thread from "../models/Thread.js";
+import Thread from "../models/thread.js";
 import getOpenAIAPIResponse from "../utils/openai.js";
 
 const router = express.Router();
